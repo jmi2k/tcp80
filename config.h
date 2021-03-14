36f97f615dc38dbaab906d0bd832032b9fbed498
José Miguel Sánchez García <soy.jmi2k@gmail.com>
2021-03-14 11:16:27 +0000

    Add MIME type for JPEG files

--- config.h
+++ config.h
@@ -4,5 +4,6 @@ char index[]	= "index.html";
 Mimetype mimetypes[] = {
 	{ ".txt",		"text/plain; charset=UTF-8" },
 	{ ".html",		"text/html; charset=UTF-8" },
+	{ ".jpeg",		"image/jpeg" },
 	{ ".png",		"image/png" },
 };
