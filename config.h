73f07c2c0c91ed11096699723ef1a797b0835cbd
José Miguel Sánchez García <soy.jmi2k@gmail.com>
2021-03-14 13:40:18 +0000

    Tweak indentation of config.h

--- config.h
+++ config.h
@@ -2,8 +2,8 @@ char server[]	= "tcp80/HEAD";
 char index[]	= "index.html";
 
 Mimetype mimetypes[] = {
-	{ ".txt",		"text/plain; charset=UTF-8" },
-	{ ".html",		"text/html; charset=UTF-8" },
-	{ ".jpeg",		"image/jpeg" },
-	{ ".png",		"image/png" },
+	{ ".txt",	"text/plain; charset=UTF-8" },
+	{ ".html",	"text/html; charset=UTF-8" },
+	{ ".jpeg",	"image/jpeg" },
+	{ ".png",	"image/png" },
 };
