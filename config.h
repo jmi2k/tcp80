8d39a40c37e352709991f3ed40dbabb590e10f54
José Miguel Sánchez García <soy.jmi2k@gmail.com>
2021-03-16 16:34:02 +0000

    Add MIME type for CSS files to config.h

--- config.h
+++ config.h
@@ -3,6 +3,7 @@ char index[]	= "index.html";
 ulong timeout	= 60*1000;
 
 Mimetype mimetypes[] = {
+	{ ".css",	"text/css; charset=UTF-8" },
 	{ ".txt",	"text/plain; charset=UTF-8" },
 	{ ".html",	"text/html; charset=UTF-8" },
 	{ ".jpeg",	"image/jpeg" },
